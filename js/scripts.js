efa4c35ee8ec0f037e8043887ca59fedd5524ced
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2016-12-31 14:02:51 +0100

    triangleArea

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+﻿var a = prompt('Podaj długość podstawy')
+var h = prompt('Podaj wysokość trójkąta')
+var triangleArea = (a*h/2)
+console.log('Pole trójkąta o podstawie a: '+a+' i wysokości h: '+h+' wynosi: '+triangleArea);
