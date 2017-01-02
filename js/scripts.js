6f4ca3fd141c5463b71a7b38173b70ad6ba773c1
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2017-01-02 23:13:26 +0100

    Wiem ze nie dziala, ale nie mam pojecia co jest nie tak

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
-var elephant = "Zielone słonie";
-var bigGreenElephant = elephant.toUpperCase();
-var animal = text.replace('Papugi', bigGreenElephant);
-console.log(animal.length);
-var partOfAnimal = animal.slice(0,(animal.length)/2);
-console.log(partOfAnimal);
+function rysujChoinkę( n = 3 ) {
+	for ( var star = "" ; for ( var n = star ; n * 2 - 1 > 0 ; star += '*')) {
+	console.log(star);
+}
+}
