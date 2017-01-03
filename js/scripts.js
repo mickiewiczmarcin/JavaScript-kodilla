daa2bf5016d94c9a535400fea48d897407f824b0
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2017-01-03 17:22:26 +0100

    wywalone spacj

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,5 @@ var elephant = "Zielone słonie";
 var bigGreenElephant = elephant.toUpperCase();
 var animal = text.replace(text.slice(0,text.indexOf(' ')), bigGreenElephant);
 console.log(animal.length);
-var partOfAnimal = animal.slice(0,(animal.length)/2);
+var partOfAnimal = animal.slice(0, animal.length / 2);
 console.log(partOfAnimal);
