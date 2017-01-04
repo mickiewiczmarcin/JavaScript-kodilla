394289ce0a0773f9ea95877b628dceb00d33c140
mickiewiczmarcin <mickiewicz.marcin@gmail.com>
2017-01-04 17:13:39 +0100

    corrected function

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
-function getTriangleArea(a, h) {	
-	
-	var triangleArea = (a * h/2)
-
-
-	if ((a && h) <= '0') {
-	console.log('Nieprawidłowe dane')
+﻿function getTriangleArea(a, h) {
+	if ( ( a && h ) > 0 ) {
+		var triangleArea = ( a * h / 2 );
+	} else {
+		console.log('Nieprawidłowe dane');
 	}
 	return triangleArea
 }
-console.log( getTriangleArea(10,6) )
-
-var triangle1Area = getTriangleArea(10, 15)
-var triangle2Area = getTriangleArea(5, 26)
-var triangle3Area = getTriangleArea(63, 5)
-
-console.log(triangle1Area)
-console.log(triangle2Area)
-console.log(triangle3Area)
+console.log( getTriangleArea(0,6) )
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(5, 26);
+var triangle3Area = getTriangleArea(63, 5);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
